f466483492237cbdf8b19d8e95e852be5a1dfce1
Erdenetsogt <erdenetsogt@gmail.com>
2025-08-27 18:05:52 +0800

    main

---
+++ types/index.tsx
@@ -0,0 +1,5 @@
+import * as auth from "./auth";
+import * as employee from "./employee";
+import * as measure from "./measure";
+
+export { auth, employee, measure };
---
+++ types/measure.ts
@@ -0,0 +1,9 @@
+export interface sensor{
+    id: number;
+    name: string;
+    description: string;
+    unit: string;
+    location: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
